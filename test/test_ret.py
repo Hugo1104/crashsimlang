956d426a32d37eb07e239f9f23db4875e3673f03
Hugo <zy1265@nyu.edu>
2021-06-21 03:01:04 -0400

    different version of test for ret

--- test/test_ret.py
+++ test/test_ret.py
@@ -29,10 +29,19 @@ class TestDefine(unittest.TestCase):
             )
         )
 
+        assert "fstat" in containerbuilder.builders
         assert "statbuf" in containerbuilder.builders
 
         container = datawords[0].container
 
+        assert container["type"] == "fstat"
+        assert container["members"][0]["type"] == "Numeric"
+        assert container["members"][0]["members"] == [3]
+        assert container["members"][1]["type"] == "statbuf"
+        assert container["members"][1]["members"][0]["type"] == "String"
+        assert "makedev(0, 4)" in container["members"][1]["members"][0]["members"][0]
+        assert container["members"][1]["members"][1]["type"] == "String"
+        assert "402" in container["members"][1]["members"][1]["members"][0]
 
 
     def test_definedup(self):
