1c8787916cfbaa6202569a7b03bb6266963fe3a6
Hugo <zy1265@nyu.edu>
2021-06-21 02:38:19 -0400

    add error for allowing a structure to look for value in ret position

--- test/test_ret.py
+++ test/test_ret.py
@@ -12,9 +12,9 @@ def get_test_data_path(filename):
 
 class TestDefine(unittest.TestCase):
     def test_define(self):
-        test_file = get_test_data_path("define.cslang")
+        test_file = get_test_data_path("ret.cslang")
         automaton, containerbuilder = cslang_main(
-            Namespace(mode="build", cslang_path=get_test_data_path("define.cslang"))
+            Namespace(mode="build", cslang_path=get_test_data_path("ret.cslang"))
         )
 
         automaton, datawords, _ = cslang_main(
@@ -28,19 +28,12 @@ class TestDefine(unittest.TestCase):
                 automaton_path=get_test_data_path("define.auto"),
             )
         )
-        assert "fstat" in containerbuilder.builders
+
         assert "statbuf" in containerbuilder.builders
 
         container = datawords[0].container
 
-        assert container["type"] == "fstat"
-        assert container["members"][0]["type"] == "Numeric"
-        assert container["members"][0]["members"] == [3]
-        assert container["members"][1]["type"] == "statbuf"
-        assert container["members"][1]["members"][0]["type"] == "String"
-        assert "makedev(0, 4)" in container["members"][1]["members"][0]["members"][0]
-        assert container["members"][1]["members"][1]["type"] == "String"
-        assert "402" in container["members"][1]["members"][1]["members"][0]
+
 
     def test_definedup(self):
         with self.assertRaises(CSlangError) as cm:
